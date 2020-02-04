69456ad13faaecf4c20886e3655447cc0f53a01a
yashgagneja <56828872+yashgagneja@users.noreply.github.com>
2020-02-04 15:55:09 +0530

    Update convertkg.py
    added buttons which are used to separate the task of each other conversion

--- convertkg.py
+++ convertkg.py
@@ -1,23 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  4 15:43:52 2020
+
+@author: yash gagneja
+"""
+
 from tkinter import *
 
 window = Tk()
 window.title("Welcome to tkinter")
 window.geometry("540x100")
+def from_kg2():
+     pound=float(e2_value.get())*2.20462
+     pound=str(str(pound)+' pound')
+     t1.delete("1.0", END)
+     t1.insert(END,pound)
+def from_kg3():
+     ounce=float(e2_value.get())*35.274
+     ounce=str(str(ounce)+' ounce')
+     t1.delete("1.0", END)
+     t1.insert(END,ounce)
 
-def from_kg():
+    
+    
+def from_kg1():
     gram=float(e2_value.get())*1000
     gram=str(str(gram)+' grams')
-    pound=float(e2_value.get())*2.20462
-    pound=str(str(pound)+' pound')
-    ounce=float(e2_value.get())*35.274
-    ounce=str(str(ounce)+' ounce')
     t1.delete("1.0", END)
     t1.insert(END,gram)
-    t2.delete("1.0", END)
-    t2.insert(END,pound)
-    t3.delete("1.0", END)
-    t3.insert(END,ounce)
-
+    
+  
 e1=Label(window,text="Kg")
 e1.grid(row=0,column=0)
 
@@ -25,16 +37,13 @@ e2_value=StringVar()
 e2=Entry(window,textvariable=e2_value)
 e2.grid(row=0,column=1)
 
-b1=Button(window,text="Convert",command=from_kg, fg='black', bg='red')
+b1=Button(window,text="Convert into gram",command=from_kg1, fg='black', bg='red')
 b1.grid(row=0,column=2)
-
+b1=Button(window,text="Convert into pound",command=from_kg2, fg='black', bg='red')
+b1.grid(row=1,column=2)
+b1=Button(window,text="Convert into ounce",command=from_kg3, fg='black', bg='red')
+b1.grid(row=2,column=2)
 t1=Text(window,height=1,width=20)
-t1.grid(row=1,column=0)
-
-t2=Text(window,height=1,width=20)
-t2.grid(row=1,column=1)
-
-t3=Text(window,height=1,width=20)
-t3.grid(row=1,column=2)
+t1.grid(row=1,column=1)
 
 window.mainloop()
